4c0ea89e222ca85a5890f67cfddc48f062248266
Sakib Rahman <srskb15@gmail.com>
2022-04-11 13:20:38 +0600

    SA | Collection router updated

--- server/routes/collection.routes.js
+++ server/routes/collection.routes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const {
     collectionController,
-    // collectionRestaurantController,
+    collectionRestaurantController,
 } = require("../controllers/collection.controller");
 router.get('/collections',collectionController);
+router.get('/collections/:id',collectionController);
 router.post('/collections',collectionController);
 router.put('/collections/:id',collectionController);
 router.delete('/collections/:id',collectionController);
+router.get('/collections/:id/restaurants',collectionRestaurantController);
+router.post('/collections/:id/restaurants',collectionRestaurantController);
+router.delete('/collections/:id/restaurants',collectionRestaurantController);
 
 module.exports = router;
