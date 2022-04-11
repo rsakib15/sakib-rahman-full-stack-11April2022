a89db6ff0523e7a2f9504a0cc8a74da60b7f8f49
Sakib Rahman <srskb15@gmail.com>
2022-04-11 15:01:01 +0600

    SA | index.js final version

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// reportWebVitals();
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import MainApp from './MainApp';
+import './Styles/index.css';
 const glints = document.getElementById('glints-site');
 
-let render = () => {
-  const MainApp = require('./MainApp').default;
-  ReactDOM.render(<MainApp/>, glints);
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>,glints
+);
