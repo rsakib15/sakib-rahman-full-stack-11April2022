05c772bb5dd5596b2cc9c1265cf2a0d34ca6dcd2
Sakib Rahman <srskb15@gmail.com>
2022-04-11 13:31:40 +0600

    SA | Collection restaurant API added

--- server/controllers/collection.controller.js
+++ server/controllers/collection.controller.js
@@ -1,4 +1,5 @@
 const client = require("../../config/db");
+var _ = require('lodash');
 
 exports.collectionController = async(req, res) => {
     if(req.method === "GET"){
@@ -20,7 +21,7 @@ exports.collectionController = async(req, res) => {
             }
         }
         else{
-            const data = await client.query(`SELECT * FROM collections;`);
+            const data = await client.query(`SELECT * FROM collections where created_by=$1;`, [req.user.id]);
             const arr = data.rows;
             if(arr.length != 0){
                 return res.status(200).json({
@@ -115,3 +116,80 @@ exports.collectionController = async(req, res) => {
 }
 
 
+exports.collectionRestaurantController = async(req, res) => {
+    if(req.method === "GET"){
+        const {id} = req.params;
+        if(id){
+            const data = await client.query(`SELECT * FROM clrestaurants where collection_id=$1;`,[id]);
+            const arr = data.rows;
+            if(arr.length != 0){
+                return res.status(200).json({
+                    success: true,
+                    data: arr
+                });
+            }
+            else{
+                return res.status(404).json({
+                    success: false,
+                    msg: "No Restaurant found in collection"
+                });
+            }
+        }
+        else{
+            return res.status(400).json({
+                success: false,
+                msg: "Missing required fields"
+            });
+        }
+    }
+    else if(req.method === "POST"){
+        const {collection_id, restaurant_id} = req.body;
+        if(collection_id && restaurant_id){
+            const data = await client.query(`INSERT INTO clrestaurants (collection_id, restaurant_id) VALUES ((select id from restaurants where id = $1), (select id from collections where id = $2)) RETURNING*`, [collection_id, restaurant_id]);
+            const arr = data.rows;
+            if(arr.length != 0){
+                return res.status(201).json({
+                    success: true,
+                    data: arr
+                });
+            }
+            else{
+                return res.status(404).json({
+                    success: false,
+                    msg: "Collection not found"
+                });
+            }
+        }
+        else{
+            return res.status(400).json({
+                success: false,
+                msg: "Missing required fields"
+            });
+        }
+    }
+    else if(req.method === "DELETE"){
+        const {id} = req.params;
+        if(id){
+            const data = await client.query(`DELETE FROM collections_restaurants WHERE id = $1 RETURNING*`, [id]);
+            const arr = data.rows;
+            if(arr.length != 0){
+                return res.status(200).json({
+                    success: true,
+                    data: arr
+                });
+            }
+            else{
+                return res.status(404).json({
+                    success: false,
+                    msg: "Collection not found"
+                });
+            }
+        }
+        else{
+            return res.status(400).json({
+                success: false,
+                msg: "Missing required fields"
+            });
+        }
+    }
+}
