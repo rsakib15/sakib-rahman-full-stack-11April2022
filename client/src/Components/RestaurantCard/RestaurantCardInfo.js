28f0d8f4c66a00d5ea475134f51fbf9732c00738
Sakib Rahman <srskb15@gmail.com>
2022-04-12 18:04:49 +0600

    SA | Restaurant Card Time changed to 12 hour

--- client/src/Components/RestaurantCard/RestaurantCardInfo.js
+++ client/src/Components/RestaurantCard/RestaurantCardInfo.js
@@ -3,14 +3,8 @@ import reslogo from "./../../assets/restaurant-logo.png";
 var _=require('lodash');
 
 const RestaurantCardInfo = ({data}) => {
-	var daylist = ["Sun","Mon","Tues","Weds","Thurs","Fri","Sat"];
-	var today = new Date();
 	var res_hours = data.hours;
-	var res_hours_today = _.groupBy(res_hours,'opening_time')
 	var result = _.groupBy(res_hours,'opening_time')
-	console.log(result)
-
-
 	var keys = Object.keys(result);
 	var res = [];
 
@@ -25,22 +19,28 @@ const RestaurantCardInfo = ({data}) => {
 			days: d
 		})
 	}
-	console.log(res);
-
+	
+	const modify12HourFormat = (time) => {
+		var t = time.split(':');
+		var hours = parseInt(t[0]);
+		var minutes = parseInt(t[1]);
+		var ampm = hours >= 12 ? 'PM' : 'AM';
+		hours = hours % 12;
+		hours = hours ? hours : 12;
+		hours = hours < 10 ? '0'+hours : hours;
+		minutes = minutes < 10 ? '0'+minutes : minutes;
+		var strTime = hours + ':' + minutes + ' ' + ampm;
+		return strTime;
+	}
 
     return(
-        <div className="flex mb-4 mr-4 shadow-lg">
+        <div className="flex mb-4 mr-4 bg-white rounded-lg border shadow-md sm:px-12 dark:bg-gray-800 dark:border-gray-700">
 			<div className="flex-none w-60 relative">
 				<img src={reslogo} alt="" className="absolute inset-0 w-full h-full object-cover" />
   			</div>
 			<form className="flex-auto p-6">
 				<div className="flex flex-wrap">
 					<h1 className="flex-auto text-xl font-semibold">{data.name}</h1>
-					<div className="text-xl font-semibold text-gray-500">
-						{
-							daylist[today]==data.opening_time < Date.now() ? <span className="text-rose-500">Closed</span> : data.closing_time > Date.now() ? <span className="text-green-800">Open</span> : <span className="text-rose-500">Closed</span>
-						}
-					</div>
 					<div className="w-full flex-none text-sm font-medium text-gray-500 mt-0">Restaurant</div>
 				</div>
 				<div className="flex items-baseline mt-4 mb-6">
@@ -60,8 +60,8 @@ const RestaurantCardInfo = ({data}) => {
 											return (
 											<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
 												<td scope="row" className="px-14 py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">{item.days.length>1 ? item.days[0] + " - " + item.days[item.days.length-1] : item.days[0]}</td>
-												<td className="px-14 py-1">{item.opening_time}</td>
-												<td className="px-14 py-1">{item.closing_time}</td>
+												<td className="px-14 py-1">{modify12HourFormat(item.opening_time)}</td>
+												<td className="px-14 py-1">{modify12HourFormat(item.closing_time)}</td>
 											</tr>
 											)
 										})
