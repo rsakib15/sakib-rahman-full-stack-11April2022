d4a0c6f1b243f9d9bacda32fb4e1e0e6632a1bd1
Sakib Rahman <srskb15@gmail.com>
2022-04-12 16:26:39 +0600

    SA | Overflow problem fixed for some page

--- client/src/Containers/App.js
+++ client/src/Containers/App.js
@@ -131,7 +131,7 @@ const App = (props) => {
 
 	return(
 		<div>
-			<div className="mcontainer">
+			<div className="mcontainer ">
 				<div className="flex flex-wrap">
 					<div className="w-full lg:w-3/4">
 						{
