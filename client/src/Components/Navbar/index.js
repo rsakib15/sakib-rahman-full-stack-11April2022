b9abf9564f3002f551004570e0f961929a9b5c61
Sakib Rahman <srskb15@gmail.com>
2022-04-12 12:51:00 +0600

    SA | Login, Signup button added for Navbar

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -3,7 +3,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import Logo from "./../../assets/logo.png";
 import UserImage from "./../../assets/user.png";
-import {  NavLink  } from 'react-router-dom';
+import {  NavLink, Link  } from 'react-router-dom';
 
 const navigation = [
   { name: 'Dashboard', href: '/', current: false },
@@ -64,35 +64,9 @@ export default function Navbar() {
                   
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src={UserImage}
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (<NavLink to="/" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>Logout</NavLink>)}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+              <div className='space-x-3'>
+                <Link to="/login" className='bg-gray-900 text-white hover:bg-gray-700 hover:text-white px-5 py-2 rounded-md text-sm font-medium'>Login</Link>
+                <Link to="/registration" className="bg-gray-900 text-white hover:bg-gray-700 hover:text-white px-5 py-2 rounded-md text-sm font-medium">Sign Up</Link>
               </div>
             </div>
           </div>
