bcfd658bda602c8db9e4afd3a4a1cecb4ebe4b59
Sakib Rahman <srskb15@gmail.com>
2022-04-15 15:14:42 +0600

    SA | problem multiple class fixed

--- client/src/Components/RestaurantCard/RestaurantCardInfo.js
+++ client/src/Components/RestaurantCard/RestaurantCardInfo.js
@@ -51,7 +51,7 @@ const RestaurantCardInfo = ({data}) => {
 				<div className="mb-2 mt-2">
 					<div className="text-sm font-medium text-gray-500">
 						<div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
-							<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 w-full">
+							<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 								<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 text-center">
 									<tr className="text-center">
 										<th scope="col" className="px-5 py-1">Day</th>
